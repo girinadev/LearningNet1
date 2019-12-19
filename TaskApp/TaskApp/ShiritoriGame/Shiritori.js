a559e894c0343c6844433b093e2c81ab6cf48d2b
Irina Gershun <irinag@wizardsdev.com>
2019-12-19 10:51:05 +0200

    TaskShiritoriGame: Was added ShiritoriGame class

---
+++ TaskApp/TaskApp/ShiritoriGame/Shiritori.js
@@ -0,0 +1,39 @@
+﻿class Shiritori {
+  constructor() {
+    this.words = [];
+    this.game_over = false;
+  }
+
+  play(word) {
+    if (word && word.length > 0 && !this.isWordExist(word) && this.isWordMatchPrevWord(word)) {
+      this.words.push(word);
+      return this.words;
+    }
+
+    this.game_over = true;
+    return "game over";
+  }
+
+  restart() {
+    this.words = [];
+    this.game_over = false;
+
+    return 'game restarted';
+  }
+
+  isWordExist(word) {
+    return this.words.includes(word);
+  }
+
+  isWordMatchPrevWord(word) {
+    if (this.words.length <= 0)
+      return true;
+
+    const lastWord = this.words[this.words.length - 1];
+    return lastWord[lastWord.length - 1] === word[0];
+  }
+}
+
+Shiritori.new = function () {
+  return new Shiritori();
+}
---
+++ TaskApp/TaskApp/ShiritoriGame/main.js
@@ -0,0 +1,31 @@
+﻿my_shiritori = Shiritori.new();
+
+console.log(my_shiritori.play("apple")); //➞ ["apple"]
+console.log(my_shiritori.play("ear")); //➞ ["apple", "ear"]
+console.log(my_shiritori.play("rhino")); //➞ ["apple", "ear", "rhino"]
+console.log(my_shiritori.play("corn")); //➞ "game over"
+
+// Corn does not start with an "o".
+
+console.log(my_shiritori.words); //➞  ["apple", "ear", "rhino"]
+
+// Words should be accessible.
+
+console.log(my_shiritori.restart()); //➞ "game restarted"
+console.log(my_shiritori.words); //➞ []
+
+// Words array should be set back to empty.
+
+console.log(my_shiritori.play("hostess")); //➞ ["hostess"]
+console.log(my_shiritori.play("stash")); //➞ ["hostess", "stash"]
+console.log(my_shiritori.play("hostess")); //➞ "game over"
+
+// Words cannot have already been said.
+
+console.log(my_shiritori.play("apple")); //➞ ["apple"]
+console.log(my_shiritori.play("ear")); //➞ ["apple", "ear"]
+console.log(my_shiritori.play("rhino")); //➞ ["apple", "ear", "rhino"]
+console.log(my_shiritori.play("ocelot")); //➞ ["apple", "ear", "rhino", "ocelot"]
+console.log(my_shiritori.play("oops")); //➞ "game over"
+console.log(my_shiritori.words); //➞ ["apple", "ear", "rhino", "ocelot"]
+console.log(my_shiritori.game_over); //➞ true
