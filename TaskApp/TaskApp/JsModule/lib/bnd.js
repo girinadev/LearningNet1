eefd81fbc98a3a9fb576b79915b1c5a2af7feb82
Irina Gershun <irinag@wizardsdev.com>
2020-02-04 20:22:57 +0200

    task: up5

--- TaskApp/TaskApp/JsModule/lib/bnd.js
+++ TaskApp/TaskApp/JsModule/lib/bnd.js
@@ -5,42 +5,39 @@
     var dataBindElements = document.querySelectorAll('[data-bind]');
 
     for (const element of dataBindElements) {
-
-      const bindAttr = element.getAttribute('data-bind');
-      if (!bindAttr)
+      const attrDict = getMappingDict(element);
+      if (attrDict.length === 0)
         continue;
 
-      const bindParams = bindAttr.split(':');
-      if (bindParams.length < 2)
-        continue;
-
-      const viewModelProp = bindParams[1].trim();
-      if (!viewModel.hasOwnProperty(viewModelProp))
+      const viewModelPropKey = Object.keys(attrDict)[0];
+      const viewModelPropValue = attrDict[viewModelPropKey];
+      if (!viewModel.hasOwnProperty(viewModelPropValue))
         continue;
 
       let bindingContext;
-      if (Binder.bindingContexts[viewModelProp]) {
-        bindingContext = Binder.bindingContexts[viewModelProp];
+      if (Binder.bindingContexts[viewModelPropValue]) {
+        bindingContext = Binder.bindingContexts[viewModelPropValue];
       } else {
         bindingContext = new Binding({
           bindingModel: viewModel,
-          property: viewModelProp
+          property: viewModelPropValue
         });
-        Binder.bindingContexts[viewModelProp] = bindingContext;
+        Binder.bindingContexts[viewModelPropValue] = bindingContext;
       }
 
-      const elementEvent = mapBindingEvent(element, bindParams[0].trim());
-      if (elementEvent) {
-        bindingContext.addEventBinding(element, elementEvent, viewModelProp);
+      const elementSelect = mapBindingSelect(element, attrDict, viewModel);
+      if (elementSelect) {
+        bindingContext.addBinding(element, 'value', 'change');
         continue;
       }
 
-      //if () { //select
-      //  //todo:
-      //  continue;
-      //}
+      const elementEvent = mapBindingEvent(element, viewModelPropKey);
+      if (elementEvent) {
+        bindingContext.addEventBinding(element, elementEvent, viewModelPropValue);
+        continue;
+      }
 
-      const elementAttribute = mapBindingAttribute(element, bindParams[0].trim());
+      const elementAttribute = mapBindingAttribute(element, viewModelPropKey);
       bindingContext.addBinding(element, elementAttribute, 'keyup');
     }
 
@@ -53,10 +50,49 @@
     }
 
     function mapBindingEvent(element, attribute) {
-      const supportEvents = ["keyup", "click"];
+      const supportEvents = ["keyup", "click", "change"];
       if (supportEvents.includes(attribute))
         return attribute;
       return undefined;
+    }
+
+    function mapBindingSelect(element, attrDict, viewModel) {
+      if (!attrDict['options'])
+        return undefined;
+
+      const optionsSource = viewModel[attrDict['options']];
+      for (var optionSource of optionsSource) {
+        let optionElement = document.createElement('option');
+
+        if (attrDict['optionsText']) {
+          optionElement.innerText = optionSource[attrDict['optionsText']];
+        } else {
+          optionElement.innerText = optionSource;
+        }
+
+        if (attrDict['text']) {
+          optionElement.value = optionSource[attrDict['text']];
+        } else {
+          optionElement.value = optionSource;
+        }
+
+        element.appendChild(optionElement);
+      }
+
+      return element;
+    }
+
+    function getMappingDict(element) {
+      const attributes = element.getAttribute('data-bind');
+
+      let attrDict = {};
+
+      attributes.split(',').forEach(m => {
+        const [k, v] = m.split(':');
+        attrDict[k.trim()] = v.trim();
+      });
+
+      return attrDict;
     }
   }
 }
@@ -65,11 +101,12 @@ function Binding(b) {
   let self = this;
   this.elementBindings = [];
   this.bindingModel = b.bindingModel;
-
   this.value = b.bindingModel[b.property];
+
   this.valueGetter = function () {
     return self.value;
   };
+
   this.valueSetter = function (val) {
     self.value = val;
     for (let i = 0; i < self.elementBindings.length; i++) {
@@ -77,6 +114,7 @@ function Binding(b) {
       binding.element[binding.attribute] = val;
     }
   };
+
   this.addBinding = function (element, attribute, event) {
     let binding = {
       element: element,
@@ -92,6 +130,7 @@ function Binding(b) {
     element[attribute] = self.value;
     return self;
   };
+
   this.addEventBinding = function (element, eventName, eventFunc) {
     if (eventName && eventFunc) {
       element.addEventListener(eventName, function (event) {
@@ -107,4 +146,36 @@ function Binding(b) {
   });
 
   b.bindingModel[b.property] = this.value;
+}
+
+Date.prototype.addDays = function (days) {
+  const date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+}
+
+Date.prototype.toStringWithFormat = function () {
+  const date = new Date(this.valueOf());
+  return ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) +
+    '/' +
+    ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) +
+    '/' +
+    date.getFullYear();
+}
+
+String.prototype.parseDateWithFormat = function () {
+  const str = this.valueOf();
+
+  return Date.parse(str);
+}
+
+function parseDate(str) {
+  var mdy = str.split('/');
+  return new Date(mdy[2], mdy[0] - 1, mdy[1]);
+}
+
+function datediff(first, second) {
+  // Take the difference between the dates and divide by milliseconds per day.
+  // Round to nearest whole number to deal with DST.
+  return Math.round((second - first) / (1000 * 60 * 60 * 24));
 }
